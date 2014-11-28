32f429431f05e90e3667506d84335548798ccd5d
James Harris <computersherpa@gmail.com>
2014-11-28 17:38:46 -0600

    Connected cells

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
-	$('.cell').draggable();
+jsPlumb.ready(function(){
+	// jsplumb init code
+	jsPlumb.draggable($('.cell'));
+	jsPlumb.importDefaults({
+		// override defaults here
+		PaintStyle : {
+		  lineWidth:13,
+		  strokeStyle: 'rgba(200,0,0,0.5)'
+		},
+		DragOptions : { cursor: "crosshair" },
+		Endpoints : [ [ "Dot", { radius:7 } ], [ "Dot", { radius:11 } ] ],
+		EndpointStyles : [{ fillStyle:"#225588" }, { fillStyle:"#558822" }]
+	});
+	jsPlumb.connect({
+		source: 'C',
+		target: '3as',
+		endpoint: "Rectangle"
+	});
 });
